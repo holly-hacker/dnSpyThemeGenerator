24685ed21013c2a4490bf3d75889d571b08d6f17
HoLLy <dario.db@hotmail.com>
2020-12-28 20:55:46 +0100

    Initial commit

---
+++ dnSpyThemeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dnSpyThemeGenerator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
