526dfb44c272905663fee864e442c5c4841f08f7
HoLLy <dario.db@hotmail.com>
2020-12-31 00:52:43 +0100

    Add some colors to transfer

--- dnSpyThemeGenerator/RiderTheme.cs
+++ dnSpyThemeGenerator/RiderTheme.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -63,7 +64,6 @@ namespace dnSpyThemeGenerator
 
         private static Dictionary<string, string> ParseAttributeOption(XElement attributeOption)
         {
-
             if (attributeOption.Attribute("baseAttributes") is { } baseAttr)
             {
                 var baseName = baseAttr.Value;
@@ -95,9 +95,80 @@ namespace dnSpyThemeGenerator
             new Random(_name.GetHashCode()).NextBytes(bytes);
             donor.Guid = new Guid(bytes);
             donor.Order = 9001;
-            
+
             // TODO: colors
-            donor.Colors["string"]["fg"] = "Orange";
+            foreach ((string dnSpyColor, var dnSpyAttributes) in donor.Colors)
+            {
+                var riderColor = MapColorKey(dnSpyColor);
+                if (riderColor is null)
+                {
+                    Debug.WriteLine("Skipping unknown key " + dnSpyColor);
+                    continue;
+                }
+
+                foreach ((string dnSpyAttributeName, _) in dnSpyAttributes)
+                {
+                    var riderAttributeName = MapAttributeName(dnSpyAttributeName);
+                    if (riderAttributeName is null)
+                    {
+                        if (dnSpyAttributeName != "name")
+                            Debug.WriteLine("Skipping unknown attribute " + dnSpyAttributeName);
+                        continue;
+                    }
+
+                    dnSpyAttributes[dnSpyAttributeName] = "#" + _attributes[riderColor][riderAttributeName];
+                    Console.WriteLine($"Mapping {dnSpyColor}.{dnSpyAttributeName}");
+                }
+            }
+        }
+
+        private static string MapColorKey(string dnSpyName)
+        {
+            return dnSpyName switch
+            {
+                "defaulttext" => "TEXT",
+                "operator" => "DEFAULT_OPERATION_SIGN",
+                "punctuation" => "DEFAULT_DOT",
+                "number" => "DEFAULT_NUMBER",
+                "comment" => "DEFAULT_LINE_COMMENT",
+                "keyword" => "DEFAULT_KEYWORD",
+                "string" => "DEFAULT_STRING",
+                "verbatimstring" => "DEFAULT_STRING",
+                "char" => "DEFAULT_STRING",
+                "namespace" => "ReSharper.NAMESPACE_IDENTIFIER",
+                "type" => "DEFAULT_CLASS_NAME", // TODO
+                // TODO: sealedtype, statictype
+                "statictype" => "ReSharper.STATIC_CLASS_IDENTIFIER",
+                "delegate" => "ReSharper.DELEGATE_IDENTIFIER",
+                "enum" => "ReSharper.ENUM_IDENTIFIER",
+                "interface" => "DEFAULT_INTERFACE_NAME",
+                "valuetype" => "ReSharper.STRUCT_IDENTIFIER",
+                // TODO: module
+                "typegenericparameter" => "ReSharper.TYPE_PARAMETER_IDENTIFIER",
+                "methodgenericparameter" => "ReSharper.TYPE_PARAMETER_IDENTIFIER",
+                "instancemethod" => "DEFAULT_INSTANCE_METHOD",
+                "staticmethod" => "DEFAULT_STATIC_METHOD",
+                "extensionmethod" => "ReSharper.EXTENSION_METHOD_IDENTIFIER",
+                "instancefield" or "instanceevent" or "instanceproperty" => "DEFAULT_INSTANCE_FIELD",
+                "enumfield" or "literalfield" => "DEFAULT_INSTANCE_FIELD",
+                "staticfield" or "staticevent" or "staticproperty" => "DEFAULT_STATIC_FIELD",
+                "local" => "DEFAULT_LOCAL_VARIABLE",
+                "parameter" => "DEFAULT_PARAMETER",
+                "preprocessorkeyword" => "DEFAULT_KEYWORD",
+                // TODO: preprocessortext
+                "label" => "DEFAULT_LABEL",
+                _ => null,
+            };
+        }
+
+        private static string MapAttributeName(string dnSpyName)
+        {
+            return dnSpyName switch
+            {
+                "fg" => "FOREGROUND",
+                "bg" => "BACKGROUND",
+                _ => null,
+            };
         }
     }
 }
